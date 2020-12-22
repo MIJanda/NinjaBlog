da658617105a45935ef8cb06894687259eb8b7b4
MIJanda <mokiliisaacjanda@gmail.com>
2020-12-22 16:43:14 +0300

    sent an HTML file to the browser

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log('request made...');
+    res.setHeader('Content-Type', 'text/html');
+    // send an HTML file
+    fs.readFile('./views/index.html', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            // res.write(data);
+            res.end(data);
+        }
+    });
+
+
 });
 
 server.listen(3000, 'localhost', () => {
