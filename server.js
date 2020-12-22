742f69386a4a07f796f6ef1bfd4e120dd82f3b19
MIJanda <mokiliisaacjanda@gmail.com>
2020-12-22 16:57:35 +0300

    implemented a simple routing scheme

--- server.js
+++ server.js
@@ -3,8 +3,23 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/html');
+
+    let path = './views/';
+
+    switch(req.url) {
+        case '/':
+            path += 'index.html';
+            break;
+        case '/about':
+            path += 'about.html';
+            break;
+        default:
+            path += '404.html';
+            break;
+    }
+
     // send an HTML file
-    fs.readFile('./views/index.html', (err, data) => {
+    fs.readFile(path, (err, data) => {
         if (err) {
             console.log(err);
             res.end();
