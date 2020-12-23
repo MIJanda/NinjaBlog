0c83c52d8d0ac204016913202032b566fdaa863e
MIJanda <mokiliisaacjanda@gmail.com>
2020-12-23 19:42:39 +0300

    connected to mongodb and created a blog model

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express');
-// set up express app
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+// express app
 const app = express();
-// listen for requests 
-app.listen(3000);
+
+// connect to mongoDB
+mongoose.connect('mongodb://localhost/blog-ninja', {useNewUrlParser: true, useUnifiedTopology: true})
+        .then(() => app.listen(3000))
+        .catch(err => console.log(err));
+
+
+// middleware & static files
+app.use(express.static('public'));
+app.use(morgan('dev'));
+
 // register view engine
 app.set('view engine', 'ejs');
 
