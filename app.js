36337eaa69dafbc4d14bd9a0f6f487235e1f0af0
MIJanda <mokiliisaacjanda@gmail.com>
2020-12-23 08:11:07 +0300

    implemented ejs templates and passed data dynamically into the templates

--- app.js
+++ app.js
@@ -7,18 +7,24 @@ app.listen(3000);
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('index');
+    const blogs = [
+        {title: 'Coding and Music', snippet: 'lorem ipsum lorem ninja coding lorem ipsum music'},
+        {title: 'Production company', snippet: 'lorem ipsum lorem ninja coding lorem ipsum music'},
+        {title: 'Def Jam Music', snippet: 'lorem ipsum lorem ninja coding lorem ipsum music'}
+    ];
+
+    res.render('index', {title: 'Home', blogs});
 });
 
 app.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about', {title: 'About'});
 });
 
 app.get('/blogs/create', (req, res) => {
-    res.render('create');
+    res.render('create', {title: 'New Blog'});
 });
 
 // 404 middleware
 app.use((req, res) => {
-    res.status(404).render('404');
+    res.status(404).render('404', {title: '404'});
 });
