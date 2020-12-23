3debd6a0791e9765661f75bf94eb75c5dee93c66
MIJanda <mokiliisaacjanda@gmail.com>
2020-12-23 07:51:22 +0300

    deleted the static HTML files and replaced them with ejs files

--- app.js
+++ app.js
@@ -3,21 +3,22 @@ const express = require('express');
 const app = express();
 // listen for requests 
 app.listen(3000);
+// register view engine
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', {root: __dirname});
+    res.render('index');
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', {root: __dirname});
+    res.render('about');
 });
 
-// redirects
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+    res.render('create');
 });
 
 // 404 middleware
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', {root: __dirname});
+    res.status(404).render('404');
 });
